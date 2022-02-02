66d70211781b00162959ee4bcb5175c97422fa2d
Carlos Orellana <carlos.orellana@publimetro.cl>
2022-02-02 19:05:43 -0300

    se cambia fecha de busqueda para que inicie desde el día anterior a su ejecucion

--- analytics/makeJsonLogYesterday.php
+++ analytics/makeJsonLogYesterday.php
@@ -3,7 +3,7 @@
     require_once(ROOT_DIR.'/control/basicFunctions.php');
     require_once(ROOT_DIR.'/control/functions_analytics.php');
 
-    $yesterday = date('Y-m-d',strtotime(date('Y-m-d').' -2 days'));
+    $yesterday = date('Y-m-d',strtotime(date('Y-m-d').' -1 days'));
     $tomorrow = date('Y-m-d',strtotime(date('Y-m-d').' +1 days'));
     print_r(createLogOneDayTenDaysAgo($yesterday,$yesterday.'.json',$tomorrow));
 
